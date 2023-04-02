3e3621b9edca3aff8df97e52b4f515c5eab68fb4
VictorRaymondLucas <125387052+VictorRaymondLucas@users.noreply.github.com>
2023-04-02 17:18:53 +0200

    Added the score keeping

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0
+let computerScore = 0
 
 
 // Function that will randomly return a choice for the computer
@@ -14,16 +15,22 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return 'You tied!'
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
         return 'You won! Rock beats scissors!'
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++
         return 'You lose! Paper beats rock!'
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
         return 'You win! Paper beats rock!'
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++
         return 'You lose! Scissors beats paper!'
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
         return 'You win! Scissors beats paper!'
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++
         return 'You lose! Rock beats scissors!'
     } 
 }
@@ -35,6 +42,16 @@ function game() {
   for (let i = 0; i < 5; i++) {
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+  }
+
+  // Added this part to keep score, along with two variables at the top of the script and ++ in the playRound function
+
+  if (playerScore < computerScore) {
+    return 'You beat the computer! Hazzah!'
+  } else if (playerScore > computerScore) {
+    return 'The computer wins! Noooo!'
+  } else {
+    return 'You tied...boring!'
   }
 }
 
